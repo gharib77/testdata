d6fadc15edfdac012e66aad42661677c3a16834f
unknown <gharibjouali@gmail.com>
2023-12-05 11:57:04 +0100

    add student

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,30 @@
 import React, { useEffect, useState } from "react"
 const  App =()=> {
   const [posts,setPosts]=useState([])
+  const URL_HOST="https://testdata-server.vercel.app"
+  //const URL_HOST="http://localhost:5003/users"
   useEffect(()=>{
     const getPosts = async()=>{
-      const response = await fetch(`https://testdata-server.vercel.app/users`)
+      const response = await fetch(`${URL_HOST}/users`)
       const data = await response.json()
       setPosts(data)
     }
     getPosts()
   },[])
-  
+  const addStudent = ()=>{
+    fetch(`${URL_HOST}/create`,{
+      method:"POST",
+      headers:{"Content-Type":"application/json"},
+      body:JSON.stringify({firstname:"test1",lastname:"test2",age:34})
+    })
+    .then(response=>response.json())
+    .then(res=>console.log(res))
+
+  }
   return (
     <div>
       <p>je suis la</p>
+      <button onClick={addStudent}>create</button>
       {
         posts && posts.map(item=>(
           <p key={item.id}>{item.firstname}</p>
--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const mysql = require("mysql")
 const cors = require("cors")
 const app = express()
 require('dotenv').config()
-const port = process.env.PORT || 5002;
+const port = process.env.PORT || 5003;
 db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -23,7 +23,6 @@ db.connect((error) => {
 
 app.use(express.json())
 app.use(cors())
-console.log(process.env.DB_HOST)
 app.get("/",(req,res)=>{
   return res.send("je suis la mes amis")
 })
@@ -33,5 +32,16 @@ app.get("/users",(req,res)=>{
   })
   
 })
+app.post("/create", (req, res) => {
+  console.log("je suis la en post")
+  console.log(req.body.firstname)
+  const {firstname,lastname,age}= req.body
+  const sql = `INSERT INTO students(firstname,lastname,age) values ("${firstname}","${lastname}",${age})`;
+  const items = [firstname,lastname,age];
+  db.query(sql, (err, result) => {
+    if (err) return res.json(err);
+    return res.json({"message":"creation effectuée"});
+  });
+});
 
-app.listen(port,()=>console.log("server is runing...."))
+app.listen(port,()=>console.log(`server is runing on por ${port}`))
